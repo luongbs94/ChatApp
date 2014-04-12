3eed83dd9f9f8d9913149c756c47f47dcb1c63aa
luongbs94 <luongbs94@gmail.com>
2014-04-12 18:39:04 +0700

    first commit

---
+++ ChatApp/src/uet/chatapp/chatapp/Login.java
@@ -0,0 +1,51 @@
+package uet.chatapp.chatapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends Activity {
+
+	private EditText login_username;
+	private EditText login_password;
+	private Button login_button;
+	private TextView signup_link;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		setTitle("Login");
+		login_username = (EditText) findViewById(R.id.login_username);
+		login_password = (EditText) findViewById(R.id.login_password);
+		login_button = (Button) findViewById(R.id.login_login_button);
+		signup_link = (TextView) findViewById(R.id.login_sign_up);
+		
+		signup_link.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),SignUp.class);
+				finish();
+				startActivity(intent);
+			}
+		});
+		
+		login_button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getApplicationContext(),MainScreen.class);
+				finish();
+				startActivity(intent);
+			}
+		});
+	}
+	
+	
+
+}
